eb43dec86bf656d640c7109a2b028a124009b1b3
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2014-05-21 22:12:11 -0400

    add file to help create final_module files

---
+++ create_final_module.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import re
+
+base_path = os.path.dirname(os.path.realpath(__file__))
+dev_mod_path = base_path + '/dev_modules/'
+final_mod_path = base_path + '/final_modules/'
+for i in os.listdir(dev_mod_path):
+    if re.match('[a-z].*.py$', i):
+        src = dev_mod_path + i
+        dst = final_mod_path + i.replace('.py', '')
+        shutil.copy(src, dst)
