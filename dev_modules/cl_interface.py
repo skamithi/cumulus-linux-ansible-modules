826790a64af623b76319fb22874935162cbbd9ce
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2014-06-05 14:56:36 -0400

    docs for the beginnings of cl_interface and argument_spec done with basic option handling

---
+++ dev_modules/cl_interface.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2014, Cumulus Networks www.cumulusnetworks.com
+#
+#
+DOCUMENTATION = '''
+---
+module: cl_interface
+author: Stanley Karunditu
+short_description: Configures a front panel , bridge or bond interface.
+description:
+    - Configures a front panel, management, loopback, bridge or bond interface.
+options:
+    name:
+        description:
+            - name of the interface
+        required: true
+    ipv4:
+        description:
+            - list of IPv4 addresses to configure on the interface. use CIDR
+            syntax.
+    ipv6:
+        description:
+            - list of IPv6 addresses  to configure on the interface. use CIDR
+            syntax
+    bridgemems:
+        description:
+            - list ports associated with the bridge interface.
+
+    bondmems:
+        description:
+            - list ports associated with the bond interface
+notes:
+    - Cumulus Linux Interface Documentation - http://cumulusnetworks.com/docs/2.0/user-guide/layer_1_2/interfaces.html
+    - Contact Cumulus Networks @ http://cumulusnetworks.com/contact/
+'''
+EXAMPLES = '''
+#Example playbook entries using the cl_interface
+
+## configure a front panel port with an IP
+cl_interface: name=swp1  ipv4=10.1.1.1/24
+
+## configure a front panel port with multiple IPs
+cl_interface: name=swp1 ipv4=['10.1.1.1/24', '20.1.1.1/24']
+
+## configure a bridge interface with a few trunk members and access port
+cl_interface: name=br0  bridgemems=['swp1-10.100', 'swp11']
+
+## configure a bond interface with an IP address
+cl_interface: name=br0 bondmems=['swp1', 'swp2'] ipv4='10.1.1.1/24'
+'''
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(required=True, type='str'),
+            bridgemems=dict(type='str'),
+            bondmems=dict(type='str'),
+            ipv4=dict(type='str'),
+            ipv6=dict(type='str')
+        ),
+        mutually_exclusive=[
+            ['bridgemems', 'bondmems']
+        ]
+    )
+
+
+# import module snippets
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import *
+
+if __name__ == '__main__':
+    main()
