ac404e356125f103531c5810e0f44b29a63d2f47
Sean Cavanaugh <sean@cavanaugh.pro>
2014-09-03 15:58:37 -0400

    fixing failure commands

--- dev_modules/cl_prefix_check.py
+++ dev_modules/cl_prefix_check.py
@@ -163,13 +163,13 @@ def main():
     if _nexthop == _nonexthop and _nexthop != '':
 		module.fail_json(msg='nexthop and nonexthop cannot be the same')
 
-	#the loop
-	if loop_route_check(module):
-		_msg += 'Condition meet'
-		module.exit_json(msg=_msg, changed=True)
-	else:
-		_msg += 'Condition not met %s second timer expired' % (_timeout)
-		module.exit_json(msg=_msg, changed=False)
+    #the loop
+    if loop_route_check(module):
+	    _msg += 'Condition meet'
+	    module.exit_json(msg=_msg, changed=False)
+    else:
+	    _msg += 'Condition not met %s second timer expired' % (_timeout)
+	    module.fail_json(msg='paremeters not found')
 
 # import module snippets
 from ansible.module_utils.basic import *
