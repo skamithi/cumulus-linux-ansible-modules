405dbb4d061f94ca8a7b429030a2a0dce613e359
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2014-09-11 06:57:03 -0400

    cl_prefix_check - fix indentation and other formatting related items in test scripts

--- tests/test_cl_prefix_check.py
+++ tests/test_cl_prefix_check.py
@@ -1,26 +1,27 @@
 import mock
-from timeit import timeit
 from nose.tools import set_trace
 from dev_modules.cl_prefix_check import main, loop_route_check
 from asserts import assert_equals
 
 
 global_values = {
- 'prefix': '1.1.1.1/24',
-        'poll_interval': '1',
-        'timeout': '2',
-        'state': 'present',
-        'nexthop': '1.2.3.4',
-        'nonexthop': '1.2.3.4'
+    'prefix': '1.1.1.1/24',
+    'poll_interval': '1',
+    'timeout': '2',
+    'state': 'present',
+    'nexthop': '1.2.3.4',
+    'nonexthop': '1.2.3.4'
 }
-    
-    
+
+
 def mod_args_generator(values, *args):
-     newvalues = global_values.copy()
-     newvalues.update(values)
-     def mod_args(args):
-         return newvalues[args]
-     return mod_args
+    newvalues = global_values.copy()
+    newvalues.update(values)
+
+    def mod_args(args):
+        return newvalues[args]
+    return mod_args
+
 
 @mock.patch('dev_modules.cl_prefix_check.loop_route_check')
 @mock.patch('dev_modules.cl_prefix_check.AnsibleModule')
@@ -30,19 +31,20 @@ def test_module_args(mock_module,
     cl_prefix_check - test module arguments
     """
     instance = mock_module.return_value
-    values = {'nexthop': '1.1.1.1' } 
+    values = {'nexthop': '1.1.1.1'}
     instance.params.get.side_effect = mod_args_generator(values)
     main()
     mock_module.assert_called_with(
         argument_spec={'prefix': {'required': True, 'type': 'str'},
-                       'poll_interval': {'type':'int', 'default': 1},
+                       'poll_interval': {'type': 'int', 'default': 1},
                        'timeout': {'type': 'int', 'default': 2},
                        'state': {'type': 'str',
                                  'default': 'present',
                                  'choices': ['present', 'absent']},
                        'nexthop': {'type': 'str', 'default': ''},
                        'nonexthop': {'type': 'str', 'default': ''}
-                      })
+                       })
+
 
 @mock.patch('dev_modules.cl_prefix_check.loop_route_check')
 @mock.patch('dev_modules.cl_prefix_check.AnsibleModule')
@@ -52,7 +54,7 @@ def test_printing_module_exit_msg_loop_passed(mock_module,
     cl_prefix_check - test exit_json messages when loop check is true or false
     """
     instance = mock_module.return_value
-    values = { 'nexthop': '2.2.2.2' }
+    values = {'nexthop': '2.2.2.2'}
     instance.params.get.side_effect = mod_args_generator(values)
     # loop check is true, i.e condition is matched
     mock_loop_route_check.return_value = True
@@ -77,6 +79,7 @@ def mock_loop_check_arg(arg):
     }
     return values[arg]
 
+
 def mock_loop_check_arg_absent(arg):
     values = {
         'prefix': '10.1.1.1/24',
@@ -98,23 +101,24 @@ def test_loop_route_check_state_present(mock_module,
     """
     instance = mock_module.return_value
     instance.params.get.side_effect = mock_loop_check_arg
-    ## run_cl_cmd returns an array if there is match
-    ## returns any empty array if nothing is found.
+    # run_cl_cmd returns an array if there is match
+    # returns any empty array if nothing is found.
     mock_run_cl_cmd.return_value = ['something']
     # state is present, route is found
     assert_equals(loop_route_check(instance), True)
 
+
 @mock.patch('dev_modules.cl_prefix_check.run_cl_cmd')
 @mock.patch('dev_modules.cl_prefix_check.AnsibleModule')
 def test_loop_route_check_state_absent(mock_module,
-                                        mock_run_cl_cmd):
+                                       mock_run_cl_cmd):
     """
     cl_prefix_check - state is absent route is absent should return True
     """
     instance = mock_module.return_value
     instance.params.get.side_effect = mock_loop_check_arg_absent
-    ## run_cl_cmd returns an array if there is match
-    ## returns any empty array if nothing is found.
+    # run_cl_cmd returns an array if there is match
+    # returns any empty array if nothing is found.
     mock_run_cl_cmd.return_value = []
     # state is present, route is found
     assert_equals(loop_route_check(instance), True)
@@ -131,8 +135,8 @@ def test_loop_route_check_state_absent_route_present(mock_module,
     """
     instance = mock_module.return_value
     instance.params.get.side_effect = mock_loop_check_arg_absent
-    ## run_cl_cmd returns an array if there is match
-    ## returns any empty array if nothing is found.
+    # run_cl_cmd returns an array if there is match
+    # returns any empty array if nothing is found.
     mock_run_cl_cmd.return_value = ['something']
     # state is present, route is found
     assert_equals(loop_route_check(instance), False)
@@ -145,8 +149,8 @@ def test_loop_route_check_state_absent_route_present(mock_module,
 @mock.patch('dev_modules.cl_prefix_check.run_cl_cmd')
 @mock.patch('dev_modules.cl_prefix_check.AnsibleModule')
 def test_loop_route_check_state_present_route_failed(mock_module,
-                                        mock_run_cl_cmd,
-                                        mock_sleep):
+                                                     mock_run_cl_cmd,
+                                                     mock_sleep):
 
     """
     cl_prefix_check - state is present route is not present, timeout occurs
