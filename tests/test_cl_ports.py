9907ac47a4ac11a51faacce5ac5f7a7f28c35a31
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-02-11 15:46:07 -0500

    add and initial test function creating hash from initial ports.conf

--- tests/test_cl_ports.py
+++ tests/test_cl_ports.py
@@ -17,5 +17,18 @@ def test_module_args(mock_module):
                        'speed_4_by_10g': {'type': 'list'}})
 
 
-
-
+@mock.patch('dev_modules.cl_ports.os.path.exists')
+@mock.patch('dev_modules.cl_ports.AnsibleModule')
+def test_hash_existing_ports_conf(mock_module, mock_exists):
+    """ test putting ports.conf values into a hash """
+    # create ansiblemodule mock instance
+    instance = mock_module.return_value
+    # say that ports.conf exists
+    mock_exists.return_value = True
+    lf = open('tests/ports.conf')
+    with mock.patch('__builtin__.open') as mock_open:
+        mock_open.return_value = lf
+        cl_ports.hash_existing_ports_conf(instance)
+        mock_exists.assert_called_with('/etc/cumulus/ports.conf')
+        assert_equals(instance.existing_ports[1], '40G')
+        assert_equals(instance.existing_ports[11], '4x10G')
