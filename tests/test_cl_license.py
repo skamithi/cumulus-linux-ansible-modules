fdf77d3382c4a2368e2bae7e238998ba93a85452
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-02-14 02:55:41 -0500

    removed restart_switchd option from cl_license. updated example showing how to do this using notify..

--- tests/test_cl_license.py
+++ tests/test_cl_license.py
@@ -2,7 +2,7 @@ import mock
 from mock import MagicMock
 from nose.tools import set_trace
 from dev_modules.cl_license import license_upto_date, main, \
-    check_license_url, check_for_switchd_run_ready, get_todays_date
+    check_license_url, get_todays_date
 from asserts import assert_equals
 from datetime import date, datetime
 
@@ -76,86 +76,21 @@ def test_check_license_existence(mock_os_path_exists, mock_date):
 @mock.patch('dev_modules.cl_license.time.sleep')
 @mock.patch('dev_modules.cl_license.license_is_current')
 @mock.patch('dev_modules.cl_license.AnsibleModule')
-def test_run_main_restart_switchd_no(mock_ansible_module,
-                                     mock_license,
-                                     mock_time):
+def test_run_main(mock_ansible_module,
+                  mock_license,
+                  mock_time):
     "Test correct cl_licence file execution when restart_switchd is no"
     instance = mock_ansible_module.return_value
     instance.mock_license.return_value = True
     values = mod_args.copy()
-    values['restart_switchd'] = False
     instance.params.get.side_effect = mod_args_generator(values)
     instance.run_command.return_value = ['0', '', '']
     main()
     instance.run_command.assert_called_with(
         '/usr/cumulus/bin/cl-license -i http://10.1.1.1/license.txt',
         check_rc=True)
-    _msg = 'license updated/installed. no request to restart switchd'
+    _msg = 'license updated/installed. remember to restart switchd'
     instance.exit_json.assert_called_with(msg=_msg, changed=True)
-
-
-@mock.patch('dev_modules.cl_license.license_is_current')
-@mock.patch('dev_modules.cl_license.AnsibleModule')
-@mock.patch('dev_modules.cl_license.restart_switchd_now')
-def test_run_main_restart_switchd_yes(mock_restart_switchd,
-                                      mock_ansible_module,
-                                      mock_license):
-    """
-    Test correct cl_licence file execution when restart_switchd_is_yes\
-    and switchd fails to start. Module should run fail_json
-    """
-    mock_license.return_value = True
-    mock_restart_switchd.return_value = False
-    instance = mock_ansible_module.return_value
-    instance.params.get.side_effect = mod_args_generator(mod_args)
-    instance.run_command.return_value = ['0', '', '']
-    main()
-    instance.run_command.assert_called_with(
-        '/usr/cumulus/bin/cl-license -i http://10.1.1.1/license.txt',
-        check_rc=True)
-    instance.fail_json.assert_called_with(
-        msg='license updated/installed. switchd failed to restart')
-
-
-@mock.patch('dev_modules.cl_license.license_is_current')
-@mock.patch('dev_modules.cl_license.AnsibleModule')
-@mock.patch('dev_modules.cl_license.restart_switchd_now')
-def test_run_main_restart_switchd_yes_doesnt_fail(mock_restart_switchd,
-                                                  mock_ansible_module,
-                                                  mock_license):
-    """
-    Test correct cl_licence file execution when restart_switchd_is_yes\
-    and switchd starts properly
-    """
-    mock_license.return_value = True
-    mock_restart_switchd.return_value = True
-    instance = mock_ansible_module.return_value
-    instance.params.get.side_effect = mod_args_generator(mod_args)
-    instance.run_command.return_value = ['0', '', '']
-
-    main()
-    instance.run_command.assert_called_with(
-        '/usr/cumulus/bin/cl-license -i http://10.1.1.1/license.txt',
-        check_rc=True)
-    assert_equals(instance.fail_json.call_count, 0)
-    instance.exit_json.assert_called_with(
-        msg='license updated/installed. switchd restarted', changed=True)
-
-
-@mock.patch('dev_modules.cl_license.AnsibleModule')
-@mock.patch('dev_modules.cl_license.time.sleep')
-@mock.patch('dev_modules.cl_license.os.path.exists')
-def test_check_for_switch_running(mock_os_path_exists,
-                                  mock_time,
-                                  mock_module):
-    """
-    Test to check that it iterates for 30 seconds before failing\
-    when checking for switchd startup
-    """
-    mock_os_path_exists.return_value = False
-    check_for_switchd_run_ready(mock_module)
-    assert_equals(mock_time.call_count, 30)
-    mock_os_path_exists.assert_called_with('/var/run/switchd.ready')
 
 
 @mock.patch('dev_modules.cl_license.AnsibleModule')
