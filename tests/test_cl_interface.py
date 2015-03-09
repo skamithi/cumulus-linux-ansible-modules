8f97b30e076ad6c592261cff4368ffd149b51d04
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-03-08 23:07:50 -0400

    build desired bridge-vids config

--- tests/test_cl_interface.py
+++ tests/test_cl_interface.py
@@ -27,25 +27,32 @@ def test_module_args(mock_module,
                       },
             'ipv4': {'type': 'list'},
             'ipv6': {'type': 'list'},
-            'virtual_mac': { 'type': 'str' },
-            'virtual_ip': { 'type': 'str' },
-            'vids': { 'type': 'list' },
-            'pvid': { 'type': 'int' },
+            'virtual_mac': {'type': 'str'},
+            'virtual_ip': {'type': 'str'},
+            'vids': {'type': 'list'},
+            'pvid': {'type': 'int'},
             'speed': {'type': 'str'}}
     )
 
 
 @mock.patch('dev_modules.cl_interface.AnsibleModule')
 def test_current_iface_config(mock_module):
+    """
+    cl_interface - test getting current iface config
+    """
     mock_module.run_command = MagicMock()
     # mock AnsibleModule.run_command
-    mock_module.run_command.return_value = (1, open('tests/ifquery.json').read(), None)
+    mock_module.run_command.return_value = \
+        (1, open('tests/ifquery.json').read(), None)
     cl_int.current_iface_config(mock_module)
     current_config = mock_module.custom_curr_config.get('config')
     assert_equals(current_config.get('address'), '10.152.5.10/24')
 
 @mock.patch('dev_modules.cl_interface.AnsibleModule')
 def test_build_address(mock_module):
+    """
+    cl_interface: - test building desired address config
+    """
     mock_module.custom_desired_config = {'config': {}}
     mock_module.params = {'ipv6': ['1.1.1.1/24']}
     cl_int.build_address(mock_module)
@@ -53,4 +60,13 @@ def test_build_address(mock_module):
                   {'config': {'address': '1.1.1.1/24'}})
 
 
-
+@mock.patch('dev_modules.cl_interface.AnsibleModule')
+def test_build_vids(mock_module):
+    """
+    cl_interface - test building desired vids config
+    """
+    mock_module.custom_desired_config = {'config': {}}
+    mock_module.params = {'vids': ['1', '10-40']}
+    cl_int.build_vids(mock_module)
+    assert_equals(mock_module.custom_desired_config,
+                  {'config': {'bridge-vids': '1 10-40'}})
