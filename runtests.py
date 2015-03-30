e9fbe7d83214b3d62c7ed3621985d4dca358a7cd
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-03-30 15:09:40 -0400

    update gitignore to ignore library/__init.py__ create init.py in library directory so that nose tests complete

--- runtests.py
+++ runtests.py
@@ -6,6 +6,9 @@ import os
 
 base_path = os.path.dirname(os.path.realpath(__file__))
 mod_path = os.path.join(base_path, 'library')
+with open(os.path.join(mod_path, '__init__.py'), "w+") as f:
+    f.close()
+
 sys.path.append(os.path.dirname(mod_path))
 nose.run()
 
